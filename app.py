727402cae3c47221a400807c489413fcc2f02c67
Gabriel Salvador <gabosalvador2004@gmail.com>
2025-03-05 01:43:37 +0800

    UPDATE: simpler page structure

--- app.py
+++ app.py
@@ -1,38 +1,51 @@
 import streamlit as st
 
 # ---- PAGE SETUP ----
-homepage = st.Page(
-    page = "views/homepage.py",
-    title = "Home Page", 
-    icon = ":material/home:",
-    default = True
-)
-catalogue_page = st.Page(
-    page = "views/catalogue.py",
-    title = "Flower Catalogue", 
-    icon = ":material/local_florist:",
-)
-locator_page = st.Page(
-    page = "views/locator.py",
-    title = "Flower Shop Locator", 
-    icon = ":material/pin_drop:",
-)
-order_page = st.Page(
-    page = "views/order.py",
-    title = "Order Page", 
-    icon = ":material/shopping_cart:",
-)
-language_of_flowers_page = st.Page(
-    page = "views/language_of_flowers.py",
-    title = "Language of Flowers", 
-    icon = ":material/auto_stories:",
-)
+
+pages = [
+    st.Page(
+        page="views/homepage.py",
+        title="Home Page",
+        icon=":material/home:",
+        default=True,
+    ),
+    st.Page(
+        page="views/catalogue.py",
+        title="Flower Catalogue",
+        icon=":material/local_florist:",
+    ),
+    st.Page(
+        page="views/locator.py",
+        title="Flower Shop Locator",
+        icon=":material/pin_drop:",
+    ),
+    st.Page(
+        page="views/order.py",
+        title="Order Page",
+        icon=":material/shopping_cart:",
+    ),
+    st.Page(
+        page="views/language_of_flowers.py",
+        title="Language of Flowers",
+        icon=":material/auto_stories:",
+    ),
+]
 
 # ---- NAVIGATION SETUP ----
-st.sidebar.image("assets/Bouq-it.png", use_container_width = True)
-pg =  st.navigation(pages=[homepage,catalogue_page,locator_page,order_page,language_of_flowers_page])
+st.sidebar.image("assets/Bouq-it.png", use_container_width=True)
+pg = st.navigation(pages=pages)
 
 # ---- RUN NAVIGATION ----
 pg.run()
 
 # ---- SHARED ON ALL views ----
+
+custom_css = """
+<style>
+    .stSidebar span {
+        color: white;
+    }
+</style>
+"""
+
+st.markdown(custom_css, unsafe_allow_html=True)
