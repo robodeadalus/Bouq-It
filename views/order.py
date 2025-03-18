71c11e892889cf1d0a9d25523356919c3efb4e62
OnymMunda <jrw.munda@gmail.com>
2025-03-18 22:15:56 +0800

    integrated with database

--- views/order.py
+++ views/order.py
@@ -1,35 +1,57 @@
 import streamlit as st
+import requests
+from PIL import Image
 from sqlalchemy import select
 from dependencies.database import *
+
+db: Session = st.session_state["db"]
 
 st.title("Order Page")
 
 search_bar = st.text_input("none", placeholder="Search", label_visibility="hidden")
 
-#Available Flowers
-st.header("Available Flowers and Bouquets")
-flowerNames = ["Pastel Delight", "Pinky-pie", "Super-duper Red Flower", "Cool Flower Name"]
-flowerPrices = [50, 40, 55, 100] 
+@st.cache_data
+def fetch(url: String):
+    data = Image.open(requests.get(url, stream=True).raw)
+    return data
 
-availableFlower = st.container(key="available flower")
-with availableFlower:
-    cols = st.columns(4, gap="small", border=True)
-    for i, flower in enumerate(flowerNames):  # Placeholder for 4 flowers
-        with cols[i]:
-            st.image("https://picsum.photos/400/500")
-            st.subheader(flower)
-            st.write(f"Price: P{flowerPrices[i]}")
-            st.button("", icon=":material/add_circle:", key=f"add_{i}")
+query_available_flowers = (
+    select(Flower.name, Flower.price)
+)
+
+all_available_flowers = db.execute(query_available_flowers).all()
+
+available_flowers = st.container(key="available flowers")
+
+with available_flowers:
+    num_flowers = len(all_available_flowers)
+    num_rows = (num_flowers + 3) // 4 
+    
+    for row in range(num_rows):
+        cols = st.columns(4, gap="small", border=True)
+        
+        for col in range(4):
+            flower_index = row * 4 + col
+        
+            if flower_index < num_flowers:
+                flower, price = all_available_flowers[flower_index]
+                
+                with cols[col]:
+                    img = fetch("https://picsum.photos/400/500")
+                    st.image(img)  # Replace with actual shop images
+                    st.subheader(flower)
+                    st.write(f"₱{price: .2f}")
+                    st.button("", icon=":material/add_circle:", key=f"add_{cols[col]}")
 
 custom_css = """
 <style>
-    .st-key-available-shop .stColumn{
+    .st-key-available-flowers .stColumn{
         background-color: white;
     }
-    .st-key-available-flower [data-testid="stColumn"] {
+    .st-key-available-flowers [data-testid="stColumn"] {
         background-color: white;
     }
-    .st-key-available-flower button {
+    .st-key-available-flowers button {
         background-color: white !important;
         margin-top: -50px !important; 
         float: right;
