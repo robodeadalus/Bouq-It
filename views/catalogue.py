38665e94f9bc22d35a875f840fde2a2b64a2b49c
jaiCorpuz <jianna.corpuz@student.ateneo.edu>
2025-03-20 21:00:19 +0800

    integrated database into code

--- views/catalogue.py
+++ views/catalogue.py
@@ -1,70 +1,53 @@
 import streamlit as st
+import requests
+from PIL import Image
+from sqlalchemy import select
 
+from dependencies.database import *
+
+db: Session = st.session_state["db"]
 st.title("Flower Catalogue")
 
 st.header("Flowers")
 
-# st.markdown(
-#     """
-#     <style>
-#         /* Adjust the font size of subheaders within columns */
-#         .custom-subheader {
-#             font-size: 20px;
-#             font-weight: bold;
-#         }
-#         .dropdown {
-#             position: relative;
-#             display: inline-block;
-#         }
+@st.cache_data(show_spinner=False)
+def fetch(url: String):
+    data = Image.open(requests.get(url, stream=True).raw)
+    return data
 
-#         .dropdown-content [data-testid="stHorizontalBlock"] {
-#             display: none;
-#             position: absolute;
-#             background-color: #f9f9f9;
-#             min-width: 160px;
-#             box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-#             z-index: 1;
-#         }
-
-#         .dropdown:hover .dropdown-content {
-#             display: block;
-#         }
-
-#         .desc {
-#             padding: 15px;
-#             text-align: center;
-#         }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
+queryFlower = select(Flower.name, Flower.short_desc).limit(4)
+topFlowers = db.execute(queryFlower).all()
 
 flowers = st.container(key = "flowers")
+
 with flowers:
     col = st.columns(4, gap="small", border=True)
-    with col[0]:
-        st.image(
-            "https://catherinephillips.com.au/cdn/shop/files/E3C177A5-0F3C-4AB4-B206-8A0043849DF8.jpg?v=1717699718&width=1946"
-        )
-        st.subheader("Chrysanthemum")
-        st.markdown('<p class="dropdown-content"> Native to East Asia</p>', unsafe_allow_html=True)
-    with col[1]:
-        st.image(
-            "https://prestigebotanicals.com/cdn/shop/files/PF15-Blush-Real-Touch-Rose.jpg?v=1708631940&width=800"
-        )
-        st.subheader("Pink Rose")
-    with col[2]:
-        st.image(
-            "https://flowerranchcafe.com/cdn/shop/products/DSCF9138.jpg?v=1681202897&width=1946",
-            width=150,
-        )
-        st.subheader("Carnations")
-    with col[3]:
-        st.image(
-            "https://www.carolinefineflowers.co.za/cdn/shop/files/7_a3ed9173-5c49-4473-8abd-e27acc525abc.jpg?v=1737975360"
-        )
-        st.subheader("Sunflower")
+    i = 0
+    for flower, desc in topFlowers:
+        with col[i]:
+            img = fetch("https://picsum.photos/400/500")
+            st.image(img)
+            st.subheader(flower)
+            st.write(f"{desc}")
+        i += 1
 
+st.header("Bouquets")
+
+queryBouquet = select(Bouquet.name, Bouquet.meaning).limit(4)
+topBouquets = db.execute(queryBouquet).all()
+
+bouquets = st.container(key = "bouquets")
+
+with bouquets:
+    col = st.columns(4, gap="small", border = True)
+    i=0
+    for bouquet, meaning in topBouquets:
+        with col[i]:
+            img = fetch("https://picsum.photos/400/500")
+            st.image(img)
+            st.subheader(bouquet)
+            st.write(f"{meaning}")
+        i += 1
 
 custom_css = """
 <style>
