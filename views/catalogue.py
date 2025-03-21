90a3a75a5645cf8de10fac57986a4104bf890b43
jaiCorpuz <jianna.corpuz@student.ateneo.edu>
2025-03-21 19:43:51 +0800

    added flower and bouquet filters options

--- views/catalogue.py
+++ views/catalogue.py
@@ -8,38 +8,51 @@ from dependencies.database import *
 db: Session = st.session_state["db"]
 st.title("Flower Catalogue")
 
-st.header("Flowers")
+queryFlower = select(Flower.name, Flower.short_desc).limit(3)
+topFlowers = db.execute(queryFlower).all()
+
+flowerName = select(Flower.name)
+allFlowers = [row[0] for row in db.execute(flowerName).all()]
+
+
+queryBouquet = select(Bouquet.name, Bouquet.meaning).limit(3)
+topBouquets = db.execute(queryBouquet).all()
+
+bouquetName = select(Bouquet.name)
+allBouquets = [row[0] for row in db.execute(bouquetName).all()]
+
+allProducts = allFlowers + allBouquets
+
+
+filter = st.multiselect ("Filter By:", options=allProducts)
+
+
 
 @st.cache_data(show_spinner=False)
 def fetch(url: String):
     data = Image.open(requests.get(url, stream=True).raw)
     return data
 
-queryFlower = select(Flower.name, Flower.short_desc).limit(4)
-topFlowers = db.execute(queryFlower).all()
-
+st.header("Flowers")
 flowers = st.container(key = "flowers")
 
 with flowers:
-    col = st.columns(4, gap="small", border=True)
-    i = 0
+    col = st.columns(3, gap="small", border = True)
+    i=0
     for flower, desc in topFlowers:
         with col[i]:
             img = fetch("https://picsum.photos/400/500")
             st.image(img)
             st.subheader(flower)
             st.write(f"{desc}")
-        i += 1
+        i += 1 
+
 
 st.header("Bouquets")
-
-queryBouquet = select(Bouquet.name, Bouquet.meaning).limit(4)
-topBouquets = db.execute(queryBouquet).all()
-
 bouquets = st.container(key = "bouquets")
 
 with bouquets:
-    col = st.columns(4, gap="small", border = True)
+    col = st.columns(3, gap="small", border = True)
     i=0
     for name, meaning in topBouquets:
         with col[i]:
