0fd626a78c6b4ed101927977561e19952da10fc4
Gabriel Salvador <gabosalvador2004@gmail.com>
2025-05-05 21:42:11 +0800

    clean code

--- pages/shop_catalogue.py
+++ pages/shop_catalogue.py
@@ -1,3 +1,5 @@
+import math
+
 import requests
 import streamlit as st
 from PIL import Image
@@ -11,27 +13,39 @@ st.title("Shop Catalogue")
 
 st.header("Shops")
 
+queryShop = select(Shop).order_by(Shop.name)
+all_shops = db.execute(queryShop).scalars().all()
 
-queryShop = select(Shop.name, Shop.address, Shop.barangay, Shop.city,Shop.image_link).order_by(Shop.name)
-all_shops = db.execute(queryShop).all()
 
 shops = st.container(key="shops")
 
 with shops:
-    num_shops = len(all_shops)
-    num_rows = (num_shops + 3) // 4
+    total_shops = len(all_shops)
+    num_rows = math.ceil(total_shops / 3)
+
     for row in range(num_rows):
-        col = st.columns(4, gap="small", border=True)
-        for i in range(4):
-            shop_index = row * 4 + i
-            if shop_index < num_shops:
-                name, address, barangay, city, image_link= all_shops[shop_index]
-                with col[i]:
-                    st.image(image_link, use_container_width=True)  # Replace with actual shop images
-                    st.subheader(name)
-                    st.write(f"{address}")
-                    st.write(f"{barangay}")
-                    st.write(f"{city}")
+        cols = st.columns(3, gap="small", border=True)
+        for i, col in enumerate(cols):
+            shop_index = row * 3 + i
+            if shop_index < total_shops:
+                shop = all_shops[shop_index]
+                with col:
+                    st.image(shop.image_link, use_container_width=True)
+                    st.subheader(shop.name, anchor=False)
+                    st.write(shop.address)
+                    st.write(shop.barangay)
+                    st.write(shop.city)
+                    if st.button(
+                        "View",
+                        key=f"shop_{shop.id}",
+                        use_container_width=True,
+                        type="primary",
+                    ):
+                        st.session_state["selected_shop_id"] = shop.id
+                        st.switch_page("pages/_shop_detail.py")
+            else:
+                with col:
+                    st.write("")
 
 custom_css = """
 <style>
@@ -49,6 +63,19 @@ custom_css = """
         padding: 10px;
         border-radius: 5px;
         transition: background-color 0.3s;
+    }
+    h3 {
+        height: 5.5rem;
+        overflow: hidden;
+        white-space: pre-wrap;
+        text-overflow: ellipsis;
+        word-break: initial;
+    }
+    img {
+    height: 200px;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 10px;
     }
 </style>
 """
